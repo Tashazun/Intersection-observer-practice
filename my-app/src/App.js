88c2edc367e81a44cd9d98485ebc587157ce4541
Tasha R Zuniga <Tashazun@gmail.com>
2018-07-20 19:12:26 -0700

    functional intersection observation, used yarn and create-react-app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from "axios";
 
 class App extends Component {
 
@@ -15,26 +16,57 @@ class App extends Component {
     
     const options = {
       root: null,
-      rootMargin: 'opx',
+      rootMargin: '0%',
       threshold: 0
     };
 
-    this.observer = new IntersectionObserver(callback, options);
+    this.observer = new IntersectionObserver(this.handleObserver, options);
+    this.observer.observe(this.loadingRef);
   }
 
-  getUsers(page) {
+  handleObserver = (entities, observer) => {
+    const y = entities[0].boundingClientRect.y;
+
+    if (this.state.prevY > y) {
+      const lastUser = this.state.users[this.state.users.length - 1];
+      const curPage = lastUser.id;
+      this.getUsers(curPage);
+      this.setState({ page: curPage });
+    }
+    this.setState({ prevY: y });
+  }
+
+  getUsers = page => {
     this.setState({ loading: true });
     axios
       .get(`https://api.github.com/users?since=${page}&per_page=100`)
-      .then(rex => {
-        this.setState({ users: [...this.state.users], ...res.data });
+      .then(res => {
+        this.setState({ users: [...this.state.users, ...res.data] });
         this.setState({ loading: false });
-      })
+      });
   }
 
   render() {
+
+    const loadingCSS = {
+      height: '2rem',
+      width: '2rem'
+    }
+
+    const loadingTextCSS = { display: this.state.loading ? 'block' : 'none' };
+
     return (
-      <div className="App">
+      <div className='container'>
+        <div style={{ minHeight: '4rem'}}>
+          <ul>
+            {this.state.users.map(user => <li key={user.id}>{user.login}</li>)}
+          </ul>
+        </div>
+        <div
+          ref={loadingRef => (this.loadingRef = loadingRef)}
+          style={loadingCSS}>
+          <span style={loadingTextCSS}>Loading...</span>
+        </div>
       </div>
     );
   }
